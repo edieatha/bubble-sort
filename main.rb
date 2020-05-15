9a29ca0f12a8b30569cd8456cf756a40dd817728
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-15 16:40:51 -0600

    Add bubble sort by sorting method

--- main.rb
+++ main.rb
@@ -1,20 +1,33 @@
-def bubble_sort_by (arr)
-  for i in (0...arr.length-1)
-   for i in (0...arr.length-1)
-    if arr[i] < arr[i + 1]
-      a = arr[i]
-      arr[i] = arr[i + 1]
-      arr[i + 1] = a
-     end
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+#
+# Bubble sort and Bubble sort by
+#
+def bubble_sort(array)
+  for i in (0...array.length-1)
+    for i in (0...array.length-1)
+      array[i], array[i + 1] = array[i + 1], array[i] if arr[i] < arr[i + 1]
     end
-   end
- return arr
+  end
+  array
 end
 
+def bubble_sort_by(array)
+  for i in (0...array.length - 1)
+    for i in (0...array.length - 1)
+      value = yield(array[i], array[i + 1])
+      array[i], array[i + 1] = array[i + 1], array[i] if value.positive?
+    end
+  end
+  array
+end
 
-p bubble_sort_by ([750, 500, 300, 200, 67, 54, 23, 56, 36, 45, 120, 100, 78, 4, 3, 2, 2, 0])
-
-
-p bubble_sort_by(["hi","hello","hey"]) do |left,right| left.length - right.length end
-
-
+p bubble_sort_by([4, 3, 78, 2, 0, 2]) { |left, right| left - right}
+p bubble_sort_by([1, 1, -5, 3,-4, 5]) { |left, right| left <=> right }
+p bubble_sort_by(["hi", "hello", "hey"]) { |left, right| left.length - right.length }
+randomArray = 999.times.map{Random.rand(-99999..99999)}
+p bubble_sort_by(randomArray) {|left, right| left <=> right } == randomArray.sort ? "Test 5: Passed" : "Test 5: Failed"
+p bubble_sort_by([750, 500, 300, 200, 67, 54, 23, 56, 36, 45, 120, 100, 78, 4, 3, 2, 2, 0]) { |left, right| left - right}
+p bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
