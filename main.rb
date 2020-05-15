9e07646e44ef2dafae727c76e1ea0100da30685a
Martin Cervantes <cervantes.martine@gmail.com>
2020-05-15 17:40:57 -0600

    Fix bubble sort variable name error

--- main.rb
+++ main.rb
@@ -6,9 +6,9 @@
 # Bubble sort and Bubble sort by
 #
 def bubble_sort(array)
-  for i in (0...array.length-1)
-    for i in (0...array.length-1)
-      array[i], array[i + 1] = array[i + 1], array[i] if arr[i] < arr[i + 1]
+  for i in (0...array.length - 1)
+    for i in (0...array.length - 1)
+      array[i], array[i + 1] = array[i + 1], array[i] if array[i] < array[i + 1]
     end
   end
   array
@@ -24,10 +24,15 @@ def bubble_sort_by(array)
   array
 end
 
+random_array = 999.times.map{Random.rand(-99999..99999)}
+
+p bubble_sort([4, 3, 78, 2, 0, 2])
+p bubble_sort([1, 1, -5, 3,-4, 5])
+p bubble_sort(["hi", "hello", "hey"])
+p bubble_sort([750, 500, 300, 200, 67, 54, 23, 56, 36, 45, 120, 100, 78, 4, 3, 2, 2, 0])
+
 p bubble_sort_by([4, 3, 78, 2, 0, 2]) { |left, right| left - right}
 p bubble_sort_by([1, 1, -5, 3,-4, 5]) { |left, right| left <=> right }
 p bubble_sort_by(["hi", "hello", "hey"]) { |left, right| left.length - right.length }
-randomArray = 999.times.map{Random.rand(-99999..99999)}
-p bubble_sort_by(randomArray) {|left, right| left <=> right } == randomArray.sort ? "Test 5: Passed" : "Test 5: Failed"
+p bubble_sort_by(random_array) {|left, right| left <=> right } == random_array.sort ? "Test 5: Passed" : "Test 5: Failed"
 p bubble_sort_by([750, 500, 300, 200, 67, 54, 23, 56, 36, 45, 120, 100, 78, 4, 3, 2, 2, 0]) { |left, right| left - right}
-p bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
